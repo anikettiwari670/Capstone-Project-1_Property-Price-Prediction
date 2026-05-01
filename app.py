b5a41e8b05c5e39d73664d7d5c6ce53b6c654e8f
ANIKET TIWARI <anikettiwari670@gmail.com>
2026-05-01 23:59:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -78,7 +78,7 @@ if st.button("Predict Price"):
         prediction = model.predict(input_scaled)
 
     # Displaying the results. 
-        st.balloons()
+        st.snow()
         st.markdown(f"""
         ### Result:
         The Estimated Market Price for this Property is: 💰 ₹ {prediction[0]:,.2f} Lakhs.""")
