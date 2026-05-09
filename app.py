26bbf041ee8152b14fb85d953f05ed59df341fa5
ANIKET TIWARI <anikettiwari670@gmail.com>
2026-05-09 17:02:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -55,6 +55,14 @@ label, p, h1, h2, h3, h4, h5, h6, span {
     border: 1px solid #666 !important;
 }
 
+/* +/- Buttons */
+button[data-testid="stNumberInputStepUp"],
+button[data-testid="stNumberInputStepDown"] {
+    background-color: #2D2D44 !important;
+    color: white !important;
+    border: 1px solid #666 !important;
+}
+
 /* Dropdown Menu */
 div[role="listbox"] {
     background-color: #2D2D44 !important;
